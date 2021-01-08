77a02faeb3b8d14568fcd73964162017c56563b7
Joao Cunha <johnny.cmr@gmail.com>
2021-01-08 10:39:12 -0300

    feat: add Sound class

--- src/com/jojostudios/main/Game.java
+++ src/com/jojostudios/main/Game.java
@@ -74,6 +74,7 @@ public class Game extends Canvas implements Runnable, KeyListener, MouseListener
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
 		initFrame();
 		restart();
+//		Sound.music.play();
 	}
 	
 	public static void restart() {
