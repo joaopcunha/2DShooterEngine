81e168094fad05712239299cc792c19b163afc40
Joao Cunha <johnny.cmr@gmail.com>
2021-01-13 10:24:46 -0300

    feat: add idle and chase state to enemy class

--- src/com/jojostudios/entities/Enemy.java
+++ src/com/jojostudios/entities/Enemy.java
@@ -3,6 +3,7 @@ package com.jojostudios.entities;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 import com.jojostudios.main.Game;
 import com.jojostudios.world.Camera;
@@ -11,7 +12,8 @@ import com.jojostudios.world.World;
 public class Enemy extends Entity{
 	
 	protected double speed = 0.3;
-	protected int fieldOfView = 80;
+	protected double idleSpeed = 0.2;
+	protected int fieldOfView = 70;
 	
 	protected int maskx=8, masky=8, maskw=10, maskh=10;
 	protected int frames = 0, maxFrames = 10, animationIndex, maxAnimationIndex = 2;
@@ -22,6 +24,11 @@ public class Enemy extends Entity{
 	
 	protected int life = 10;
 	protected boolean isDamaged = false;
+	protected String state = "idle";
+	protected int idleFrames = 0, idleMaxFrames = 90;
+	protected boolean idleMooving = true;
+	
+	protected double dx = (double)(new Random().nextBoolean() ? -1 : 1), dy = (double)(new Random().nextBoolean() ? -1 : 1);
 	
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
@@ -31,35 +38,81 @@ public class Enemy extends Entity{
 		damagedSprite = Game.spritesheet.getSprite(112+32, 16, 16, 16);
 	}
 	
-	public void tick() {
-		depth = 0;
-		if(this.calculateDistance(this.getX(), this.getY(), Game.player.getX(), Game.player.getY()) < fieldOfView) {
-			if (this.isCollidingWithPlayer() == false) {
-				if ((int)x < Game.player.getX() && World.isFree((int)(x+speed), (int)y) &&
-						!isColliding((int)(x+speed), (int)y)) {
-					x+=speed;
-				} else if ((int)x > Game.player.getX() && World.isFree((int)(x-speed), (int)y) &&
-						!isColliding((int)(x-speed), (int)y)) {
-					x-=speed;
-				}
-				
-				if ((int)y < Game.player.getY() && World.isFree((int)(x), (int)(y+speed)) &&
-						!isColliding((int)(x), (int)(y+speed))) {
-					y+=speed;
-				} else if ((int)y > Game.player.getY() && World.isFree((int)(x), (int)(y-speed)) &&
-						!isColliding((int)(x), (int)(y-speed))) {
-					y-=speed;
-				}
-				
-				
-			} else {
-				if (Game.rand.nextInt(100) < 25) {
-					if (!Game.player.isInvulnerable) {
-						Game.player.life-=5;
-						Game.player.isDamaged = true;
-					}
+	public void idle() {
+		if (idleMooving) {
+			if (dx==1 && World.isFree((int)(x+speed), (int)y)) {
+				x+=dx*(idleSpeed);
+			} else if (dx==-1 && World.isFree((int)(x-speed), (int)y)) {
+				x+=dx*(idleSpeed);
+			}
+			
+			if (dy==1 && World.isFree((int)(x), (int)(y+speed))) {
+				y+=dy*(idleSpeed);
+			} else if (dy==-1 && World.isFree((int)(x), (int)(y-speed))) {
+				y+=dy*(idleSpeed);
+			}
+			
+			idleFrames++;
+			if(idleFrames == idleMaxFrames) {
+				idleFrames = 0;
+				dx = 0;
+				dy = 0;
+				idleMooving = false;
+			}
+			
+		} else {
+			idleFrames++;
+			if(idleFrames == idleMaxFrames) {
+				idleFrames = 0;
+				dx = (double) (new Random().nextBoolean() ? -1 : 1);
+				dy = (double) (new Random().nextBoolean() ? -1 : 1);
+				idleMooving = true;
+			}
+		}
+	}
+	
+	public void chase() {
+		if (this.isCollidingWithPlayer() == false) {
+			if ((int)x < Game.player.getX() && World.isFree((int)(x+speed), (int)y) &&
+					!isColliding((int)(x+speed), (int)y)) {
+				x+=speed;
+			} else if ((int)x > Game.player.getX() && World.isFree((int)(x-speed), (int)y) &&
+					!isColliding((int)(x-speed), (int)y)) {
+				x-=speed;
+			}
+			
+			if ((int)y < Game.player.getY() && World.isFree((int)(x), (int)(y+speed)) &&
+					!isColliding((int)(x), (int)(y+speed))) {
+				y+=speed;
+			} else if ((int)y > Game.player.getY() && World.isFree((int)(x), (int)(y-speed)) &&
+					!isColliding((int)(x), (int)(y-speed))) {
+				y-=speed;
+			}
+			
+			
+		} else {
+			if (Game.rand.nextInt(100) < 25) {
+				if (!Game.player.isInvulnerable) {
+					Game.player.life-=5;
+					Game.player.isDamaged = true;
 				}
 			}
+		}
+	}
+	
+	public void tick() {
+		depth = 0;
+		
+		if (state == "idle") {
+			idle();
+		} else if (state == "chase") {
+			chase();
+		}
+		
+		if(this.calculateDistance(this.getX(), this.getY(), Game.player.getX(), Game.player.getY()) < fieldOfView) {
+			state = "chase";
+		} else {
+			state = "idle";
 		}
 		
 		frames++;
